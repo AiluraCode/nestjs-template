5ed4e2dfc414b3a0c0a5186fe632c0d600ae0214
Pablo Santiago Guerra <61328784+SiddharthaGF@users.noreply.github.com>
2024-03-19 03:28:32 -0500

    init

---
+++ src/app.module.ts
@@ -0,0 +1,45 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LoggerModule } from 'nestjs-pino';
+import { ConfigModule } from '@nestjs/config';
+import { CorrelationIdMiddleware } from './middelwares/correlation-id/correlation-id.middleware';
+import { Request } from 'express';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    LoggerModule.forRoot({
+      pinoHttp: {
+        transport:
+          process.env.NODE_ENV === 'development'
+            ? {
+                target: 'pino-pretty',
+                options: {
+                  messageKey: 'message',
+                },
+              }
+            : undefined,
+        messageKey: 'message',
+        customProps: (req: Request) => ({
+          correlationId: req['x-Correlation-Id'],
+        }),
+        serializers: {
+          req: () => {
+            return undefined;
+          },
+          res: () => {
+            return undefined;
+          },
+        },
+      },
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(CorrelationIdMiddleware).forRoutes('*');
+  }
+}
---
+++ src/main.ts
@@ -0,0 +1,17 @@
+import { NestFactory } from '@nestjs/core';
+import {
+  NestFastifyApplication,
+  FastifyAdapter,
+} from '@nestjs/platform-fastify';
+import { AppModule } from './app.module';
+import { Logger } from 'nestjs-pino';
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter(),
+  );
+  app.useLogger(app.get(Logger));
+  await app.listen(3000);
+}
+bootstrap();
